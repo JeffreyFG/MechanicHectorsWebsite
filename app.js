de89fbbcdd8e6f0747acc4336cda75590333c29a
Jeffrey Fulmer Gardner <jeffreyfulmergardner@Outlook.com>
2021-07-24 16:11:49 -0700

    still working on email function

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const LandingPageRouter = require('./routes/LandingPageRouter');
 const ReviewsPageRouter = require('./routes/ReviewsPageRouter');
 const ContactPageRouter = require('./routes/ContactPageRouter');
 const app = express();
-
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
--- routes/ContactPageRouter.js
+++ routes/ContactPageRouter.js
@@ -1,15 +1,47 @@
 const { request } = require('express');
 var express = require('express');
 var router = express.Router();
-const url = require('url');    
-/* GET Contact page. */
+var nodemailer = require('nodemailer');
+const url = require('url');  
+require('dotenv').config();
+EmailUserName=process.env.EmailUserName;
+EmailPassword=process.env.EmailPassword;
+let transporter =nodemailer.createTransport({
+  host: "hectorthemechanic.site",
+  port: 465,
+  secure: true,
+  auth: {
+    user: EmailUserName,
+    pass: EmailPassword
+  }
+});
 let rootPath = { root: './public'}
+/* GET Contact page. */
 router.get('/', function(request, response, next) 
 {
   response.sendFile( './html/contact.html',rootPath);
 });
 router.post('/submit-contact-info', function(request, response, next) 
 {
+  transporter.sendMail({
+    from: '"Hector" <apointmentcreator@hectorthemechanic.site>', // sender address
+    to: "JeffreyFulmerGardner@Outlook.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  transporter.verify(function(error, success) 
+  {
+    if (error) 
+    {
+      console.log(error);
+    } 
+    else 
+    {
+      
+    }
+  });
   response.redirect(url.format({
     pathname:"/contact/contact-info-submited",
     query:request.body,
